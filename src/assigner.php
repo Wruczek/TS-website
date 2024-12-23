3935c49ae936625e3cffd624e87632f08607d2a6
toster234 <contact@toster.dev>
2024-12-23 04:18:46 +0100

    Added assginer Cooldown

--- src/assigner.php
+++ src/assigner.php
@@ -2,9 +2,20 @@
 
 use Wruczek\TSWebsite\Assigner;
 use Wruczek\TSWebsite\Auth;
+use Wruczek\TSWebsite\Utils\TeamSpeakUtils;
 use Wruczek\TSWebsite\Utils\TemplateUtils;
 
 require_once __DIR__ . "/private/php/load.php";
+
+session_start();
+
+// Fetch cooldown period from the database
+$cooldownPeriod = Assigner::getCooldownPeriod();
+
+if (!TeamSpeakUtils::i()->checkTSConnection()) {
+    TemplateUtils::i()->renderTemplate("assigner");
+    exit;
+}
 
 $data = [
     "isLoggedIn" => Auth::isLoggedIn()
@@ -14,7 +25,19 @@ if (Auth::isLoggedIn()) {
     $canUseAssigner = Assigner::canUseAssigner();
     $data["canUseAssigner"] = $canUseAssigner;
 
-    if (isset($_POST["assigner"]) && $canUseAssigner) {
+    // Check if the user is on cooldown
+    $lastUsageTime = $_SESSION['lastAssignerUsage'] ?? 0;
+    $currentTime = time();
+    $timeSinceLastUsage = $currentTime - $lastUsageTime;
+
+    if ($timeSinceLastUsage < $cooldownPeriod) {
+        $data["onCooldown"] = true;
+        $data["cooldownRemaining"] = $cooldownPeriod - $timeSinceLastUsage;
+    } else {
+        $data["onCooldown"] = false;
+    }
+
+    if (isset($_POST["assigner"]) && $canUseAssigner && !$data["onCooldown"]) {
         $groups = array_keys($_POST["assigner"]); // get all group ids
         $groups = array_filter($groups, "is_int"); // only keep integers
 
@@ -25,6 +48,8 @@ if (Auth::isLoggedIn()) {
             // if groups have been successfully updated,
             // invalidate the cache
             Auth::invalidateUserGroupCache();
+            // Update the last usage time
+            $_SESSION['lastAssignerUsage'] = $currentTime;
         }
     }
 
